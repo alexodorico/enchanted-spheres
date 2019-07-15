dffcb16aab8f3c9448e3f4cf3c994c654800c360
alexodorico <alex.odorico@morleynet.com>
2019-07-15 07:17:11 -0400

    Declared initial state

--- src/javascript/reducers.js
+++ src/javascript/reducers.js
@@ -1,75 +1,51 @@
-const START_GAME = "START_GAME";
-function startGame(start) {
-  return {
-    type: START_GAME,
-    start
-  }
-}
-
-const SHUFFLE_DECK = "SHUFFLE_DECK";
-function shuffleDeck(shuffle) {
-  return {
-    type: SHUFFLE_DECK,
-    shuffle
-  }
-}
-
-const DEAL_CARDS = "DEAL_CARDS";
-function dealCards(deal) {
-  return {
-    type: DEAL_CARDS,
-    deal
-  }
-}
-
-const ORDER_UP = "ORDER_UP";
-function orderUp(player, card) {
-  return {
-    type: ORDER_UP,
-    player,
-    card
-  }
-}
-
-const DECLARE_TRUMP = "DECLARE_TRUMP";
-function declareTrump(player, trump) {
-  return {
-    type: DECLARE_TRUMP,
-    player,
-    trump
-  }
-}
-
-const PASS_TURN = "PASS_TURN";
-function passTurn(player) {
-  return {
-    type: PASS_TURN,
-    player
-  }
-}
-
-const PLAY_CARD = "PLAY_CARD";
-function playCard(player, card) {
-  return {
-    type: PLAY_CARD,
-    player,
-    card
-  }
-}
-
-const UPDATE_SCORE = "UPDATE_SCORE";
-function updateScore(team, amount) {
-  return {
-    type: UPDATE_SCORE,
-    team,
-    amount
-  }
-}
-
-const END_GAME = "END_GAME";
-function endGame(end) {
-  return {
-    type: END_GAME,
-    end
+const initialState = {
+  gameStarted: false,
+  gameEnded: false,
+  players: {
+    first: {
+      id: 1,
+      name: new String(),
+      team: "black"
+    },
+    second: {
+      id: 2,
+      name: new String(),
+      team: "red"
+    },
+    third: {
+      id: 3,
+      name: new String(),
+      team: "black"
+    },
+    fourth: {
+      id: 4,
+      name: new String(),
+      team: "red"
+    }
+  },
+  matchScore: {
+    black: 0,
+    red: 0
+  },
+  roundNumber: 0,
+  roundScore: {
+    black: 0,
+    red: 0
+  },
+  deckOfCards: new Array(),
+  dealer: new String(),
+  hands: {
+    first: new Array(),
+    second: new Array(),
+    third: new Array(),
+    fourth: new Array()
+  },
+  trump: new String(),
+  goingAlone: undefined,
+  playersTurn: undefined,
+  cardLed: new Object(),
+  cardPlayed: {
+    card: new Object(),
+    player: new String()
   }
 }
