39f5db8010608db4383aff281b6c4982536ba0c6
alexodorico <alex.odorico@morleynet.com>
2019-07-15 20:23:37 -0400

    Added update life, move player

--- src/javascript/actions.js
+++ src/javascript/actions.js
@@ -4,7 +4,8 @@ export function playerJoined(name) {
     type: PLAYER_JOINED,
     name
   }
-  
+}
+
 export const PLAYER_LEFT = "PLAYER_LEFT";
 export function playerLeft(name) {
   return {
@@ -21,48 +22,6 @@ export function startGame(start) {
   }
 }
 
-export const SHUFFLE_DECK = "SHUFFLE_DECK";
-export function shuffleDeck(shuffle) {
-  return {
-    type: SHUFFLE_DECK,
-    shuffle
-  }
-}
-
-export const DEAL_CARDS = "DEAL_CARDS";
-export function dealCards(deal) {
-  return {
-    type: DEAL_CARDS,
-    deal
-  }
-}
-
-export const ORDER_UP = "ORDER_UP";
-export function orderUp(player, card) {
-  return {
-    type: ORDER_UP,
-    player,
-    card
-  }
-}
-
-export const DECLARE_TRUMP = "DECLARE_TRUMP";
-export function declareTrump(player, trump) {
-  return {
-    type: DECLARE_TRUMP,
-    player,
-    trump
-  }
-}
-
-export const PASS_TURN = "PASS_TURN";
-export function passTurn(player) {
-  return {
-    type: PASS_TURN,
-    player
-  }
-}
-
 export const PLAY_CARD = "PLAY_CARD";
 export function playCard(player, card) {
   return {
@@ -72,11 +31,20 @@ export function playCard(player, card) {
   }
 }
 
-export const UPDATE_SCORE = "UPDATE_SCORE";
-export function updateScore(team, amount) {
+export const MOVE_PLAYER = "MOVE_PLAYER";
+export function movePlayer(player, card) {
   return {
-    type: UPDATE_SCORE,
-    team,
+    type: MOVE_PLAYER,
+    player,
+    card
+  }
+}
+
+export const UPDATE_LIFE = "UPDATE_SCORE";
+export function updateLife(team, amount) {
+  return {
+    type: UPDATE_LIFE,
+    player,
     amount
   }
 }
