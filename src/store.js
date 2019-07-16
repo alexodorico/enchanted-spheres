e017d895350505e632e81514392d4b51090ffc3d
Alex Odorico <alex.odorico@morleynet.com>
2019-07-16 09:46:50 -0400

    Made mutations and state for a player

--- src/store.js
+++ src/store.js
@@ -3,8 +3,50 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+function Player(dimensions, firstTurn) {
+  return {
+    state: {
+      health: 3,
+      hand: new Array(),
+      position: dimensions,
+      spells: true,
+      turn: firstTurn,
+      priority: firstTurn
+    },
+    mutations: {
+      toggle(state, property) {
+        state[property] = !state[property];
+      },
+      updateHealth(state, amount) {
+        state.health = state.health + amount;
+      },
+      updatePosition(state, coordinates) {
+        state.position = coordinates;
+      },
+      updateHand(state, cardName, action) {
+        if (action === "add") {
+          state.hand.push(cardName);
+        } else {
+          state.hand = state.hand.filter(card => card !== cardName);
+        }
+      }
+    },
+    actions: {},
+    getters: {}
+  };
+}
+
 export default new Vuex.Store({
-  state: {},
+  state: {
+    gameStarted: false,
+    gameEnded: false,
+    playersJoined: 0,
+    black: Player([0, 0], true),
+    white: Player([6, 6], false),
+    stack: new Array(),
+    history: new Array()
+  },
+  getters: {},
   mutations: {},
   actions: {}
 });
