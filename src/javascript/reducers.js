d39392940ae99d263ffaea1dac24651a3faefc92
alexodorico <alex.odorico@morleynet.com>
2019-07-15 20:34:12 -0400

    Added playCard function

--- src/javascript/reducers.js
+++ src/javascript/reducers.js
@@ -1,4 +1,4 @@
-import { UPDATE_LIFE, PLAY_CARD } from "./actions";
+import { UPDATE_LIFE, PLAY_CARD, playCard } from "./actions";
 
 const initialState = {
   gameStarted: false,
@@ -43,8 +43,32 @@ function endgame(state = initialState, action) {
           lives: state[player].lives + action.amount
         }
       });
+
+      case PLAY_CARD:
+        return playCard(state, action);
     
     default:
       return state;
+  }
+}
+
+function playCard(state, action) {
+  switch(action.card) {
+    case "COUNTER_ATTACK":
+      return counterAttack(state, action);
+    case "COUNTER_SPELL":
+      return counterSpell(state, action);
+    case "BLOCK":
+      return block(state, action);
+    case "FREEZE":
+      return freeze(state, action);
+    case "STUTTER":
+      return stutter(state, action);
+    case "TIME_WARP":
+      return timeWarp(state, action);
+    case "CONFUSION":
+      return confusion(state, action);
+    case "SHORTCUT":
+      return shortcut(state, action);
   }
 }
