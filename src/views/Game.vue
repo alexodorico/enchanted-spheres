b0fdeeefa87e152dc4b6d4cddffa6b54348f0e68
Alex Odorico <alex.odorico@morleynet.com>
2019-07-16 10:24:01 -0400

    Tweaked a little

---
+++ src/views/Game.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <Test/>
+    <button @click="startGame">Start Game</button>
+    <button @click="toggleTurn">Toggle turn</button>
+    <button @click="togglePriority">Toggle priority</button>
+    <button @click="updateHealth">update health</button>
+    <button>Add card</button>
+    <button>Remove card</button>
+    <button>Attack</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Test from "@/components/Test.vue";
+
+export default {
+  name: "game",
+  components: {
+    Test
+  },
+  methods: {
+    startGame: function() {
+      this.$store.commit("toggleBoolean", "gameStarted");
+    },
+    toggleTurn: function() {
+      this.$store.commit("togglePlayer", "turn");
+    },
+    togglePriority: function() {
+      this.$store.commit("togglePlayer", "priority");
+    },
+    updateHealth: function() {
+      this.$store.commit("updateHealth", { player: "white", amount: -1 });
+    }
+  }
+};
+</script>
