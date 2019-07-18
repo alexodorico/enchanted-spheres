8a90347a4a27c548fd381c78eef3b4e15b2a785e
Alex Odorico <alex.odorico@morleynet.com>
2019-07-18 11:34:47 -0400

    Updated game state mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,19 +5,12 @@ const mutations = {
     state.gameStarted = true;
   },
 
-  endGame(state, payload) {
-    state.gameEnded = true;
-    state.winner = payload.winner;
-  },
-
-  updateStack(state, payload) {
+  addActionToStack(state, payload) {
     state.stack.unshift(payload);
   },
 
-  organicMove(state, payload) {
-    state.history[payload.user].unshift(payload.coordinates);
-    state.turnPhase = 2;
-    state.position[payload.user] = payload.coordinates;
+  removeActionFromStack(state) {
+    state.stack.shift();
   },
 
   incrementStackPhase(state) {
@@ -32,64 +25,13 @@ const mutations = {
     state.stackPhase = 0;
   },
 
-  goToTurnPhaseOne(state) {
-    state.turnPhase = 1;
+  resetTurnPhase(state) {
+    state.turnPhase = 0;
   },
 
-  toggle(state, payload) {
-    const property = payload.property;
-    state[property] = swap(state[property]);
-  },
-
-  attack(state, payload) {
-    const user = swap(payload.user);
-    state.health[user] = state.health[user] - 1;
-  },
-
-  removeCardFromHand(state, payload) {
-    const user = payload.user;
-    state.hand[user] = state.hand[user].filter(card => card !== payload.name);
-  },
-
-  counterAttack(state, payload) {
-    const user = swap(payload.user);
-    state.health[user] = state.health[user] - 1;
-  },
-
-  counterSpell(state) {
-    state.stack.shift();
-  },
-
-  removeFrozen(state, payload) {
-    state.frozen[payload.player] = false;
-  },
-
-  freeze(state, payload) {
-    const user = swap(payload.user);
-    state.frozen[user] = true;
-  },
-
-  block(state) {
-    state.stack.shift();
-  },
-
-  teleport(state, payload) {
-    state.history[payload.user].unshift(payload.coordinates);
-    state.position[payload.user] = payload.coordinates;
-  },
-
-  retreat(state, payload) {
-    state.positions[payload.user] = state.history[payload.user][1];
-  },
-
-  stutter(state) {
-    state.positions.black = state.history.black[1];
-    state.positions.white = state.history.white[1];
-  },
-
-  timeWarp(state) {
-    state.positions.black = [0, 0];
-    state.positions.white = [6, 6];
+  endGame(state, payload) {
+    state.gameEnded = true;
+    state.winner = payload.winner;
   }
 };
 
