f9cacdd7c2f55cb4b45e7abaed43095f20ade66c
Alex Odorico <alex.odorico@morleynet.com>
2019-07-19 12:48:54 -0400

    Added Controls component

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
-    <Row v-for="i in 7" :key="i - 1" :indexY="i - 1" />
+    <Row v-for="i in 7" :key="i - 1" :indexY="i - 1"/>
+    <Controls :user="this.$store.state.white.priority ? 'white' : 'black'"/>
   </div>
 </template>
 
 <script>
 import Row from "@/components/Row";
+import Controls from "@/components/Controls";
 
 export default {
   components: {
-    Row
+    Row,
+    Controls
   }
 };
 </script>
