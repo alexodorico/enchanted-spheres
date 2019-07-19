74013e20cfe93f05d9320879fbcd1239e135b7bf
Alex Odorico <alex.odorico@morleynet.com>
2019-07-19 12:46:12 -0400

    Added in spells in intial hand

--- src/store/modules/playerState.js
+++ src/store/modules/playerState.js
@@ -1,7 +1,16 @@
 const state = (dimensions, firstTurn) => {
   return {
     health: 3,
-    hand: new Array(),
+    hand: [
+      { name: "teleport", pretty: "Teleport" },
+      { name: "confusion", pretty: "Confusion" },
+      { name: "retreat", pretty: "Retreat" },
+      { name: "block", pretty: "Block" },
+      { name: "counterSpell", pretty: "Counter Spell" },
+      { name: "stutter", pretty: "Stutter" },
+      { name: "timeWarp", pretty: "Time Warp" },
+      { name: "counterAttack", pretty: "Counter Attack" }
+    ],
     position: dimensions,
     confused: false,
     turn: firstTurn,
