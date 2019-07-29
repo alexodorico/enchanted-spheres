30e55e2cf6364d067a27a4490a99ec56bac4cd55
Alex Odorico <alex.odorico@morleynet.com>
2019-07-29 09:12:18 -0400

    Moved findGame method from Home to App

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <div id="app">
-    <router-view @socket-connection="setSocket" :socket="socket"/>
+    <router-view @find-game="findGame" :socket="socket"/>
   </div>
 </template>
 <script>
+import io from "socket.io-client";
+
 export default {
   data() {
     return {
       socket: new Object()
-    }
+    };
   },
   methods: {
-    setSocket: function(socket) {
-      this.socket = socket;
+    findGame: function(socket) {
+      fetch("http://localhost:3000/joingame")
+        .then(response => response.json())
+        .then(response => {
+          const socket = io(`http://localhost:3000/${response.id}`);
+          this.socket = socket;
+
+          this.$store.commit("setPlayerColor", { color: response.color });
+          this.$router.push(`/game/${response.id}`);
+
+          socket.on("action", message => {
+            this.$store.dispatch(message.action, message.payload);
+          });
+        });
     }
   }
-}
+};
 </script>
 <style lang="scss">
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h1>Enchanted Stones</h1>
+    <!-- <h1>Enchanted Stones</h1> -->
     <button @click="findGame">Find Game</button>
-    <button><router-link to="/instructions">Instructions</router-link></button>
+    <button>
+      <router-link to="/instructions">Instructions</router-link>
+    </button>
   </div>
 </template>
 
@@ -10,34 +12,13 @@
 import io from "socket.io-client";
 
 export default {
-  data() {
-    return {
-      socket: "",
-      gameId: 0,
-    }
-  },
   methods: {
     findGame: function() {
-      fetch("http://localhost:3000/joingame")
-        .then(response => response.json())
-        .then(response => {
-          let socket = io(`http://localhost:3000/${response.id}`);
-          console.log(socket);
-
-          this.$emit("socket-connection", socket);
-          this.gameId = response.id;
-          this.$store.commit("setPlayerColor", {color: response.color});
-          this.$router.push(`/game/${response.id}`);
-
-          socket.on("action", function(msg) {
-            console.log(msg);
-          });
-        });
+      this.$emit("find-game");
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
