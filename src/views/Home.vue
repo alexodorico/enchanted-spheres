d4a4b31a8c44c5599c3ba505fba84645ee47e1bb
alexodorico <alex.odorico@morleynet.com>
2019-07-29 06:45:18 -0400

    Added routes for home and instructions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,39 @@
 <template>
-  
+  <div>
+    <h1>Enchanted Stones</h1>
+    <button @click="findGame">Find Game</button>
+    <button><router-link to="/instructions">Instructions</router-link></button>
+  </div>
 </template>
 
 <script>
-export default {
+import io from "socket.io-client";
 
+export default {
+  data() {
+    return {
+      socket: "",
+      gameId: 0,
+    }
+  },
+  props: {
+    color: String
+  },
+  methods: {
+    findGame: function() {
+      fetch("http://localhost:3000/joingame")
+        .then(response => response.json())
+        .then(response => {
+          this.socket = io(`/${response.id}`);
+          this.gameId = response.id;
+          this.$store.commit("setPlayerColor", {color: response.color});
+          this.$router.push(`/game/${response.id}`)
+          this.socket.on("action", function(msg) {
+            console.log(msg)
+          });
+        });
+    }
+  }
 }
 </script>
 
