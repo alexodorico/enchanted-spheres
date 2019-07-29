d2151338de1122d28ff9f1ff60c55e887546a819
alexodorico <alex.odorico@morleynet.com>
2019-07-29 06:44:34 -0400

    Added routes for home and instructions

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Instructions from "./views/Instructions.vue";
 import Game from "./views/Game.vue";
 
 Vue.use(Router);
@@ -10,6 +12,16 @@ export default new Router({
   routes: [
     {
       path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/instructions",
+      name: "instructions",
+      component: Instructions
+    },
+    {
+      path: "/game/:id",
       name: "game",
       component: Game
     }
